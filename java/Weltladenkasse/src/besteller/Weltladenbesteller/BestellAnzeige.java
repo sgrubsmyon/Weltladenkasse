3cb4e3de10ebcbdd5d4ed2c8901f662fbdc0af1c
sgrubsmyon <markus.voge@ymail.com>
2014-08-01 01:18:08 +0200

    Alse the UPDATE statements moved to PreparedStatement (were already!)

--- java/Weltladenkasse/src/besteller/Weltladenbesteller/BestellAnzeige.java
+++ java/Weltladenkasse/src/besteller/Weltladenbesteller/BestellAnzeige.java
@@ -19,7 +19,7 @@ import java.awt.*;
 //import java.awt.event.ActionEvent;
 //import java.awt.event.ActionListener;
 import java.awt.event.*;
- 
+
 //import javax.swing.JFrame;
 //import javax.swing.JPanel;
 //import javax.swing.JScrollPane;
