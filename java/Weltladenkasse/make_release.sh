b70ea32d611b1d33fae57f51dee6d0432ca5a50d
sgrubsmyon <markus.voge@ymail.com>
2014-08-17 16:29:21 +0200

    Also copy Artikelliste.csv for a release

--- java/Weltladenkasse/make_release.sh
+++ java/Weltladenkasse/make_release.sh
@@ -10,6 +10,7 @@ rsync -aPvci Weltladenkasse_$version.jar $releasedir
 rsync -aPvci Weltladenbesteller_$version.jar $releasedir
 rsync -aPvci Bestellvorlage.ods $releasedir
 rsync -aPvci Artikelliste.ods $releasedir
+rsync -aPvci Artikelliste.csv $releasedir
 rsync -aPvci README.txt $releasedir
 rsync -aPvci install-ubuntu.sh $releasedir
 rsync -aPvci --exclude=".*" mysql $releasedir
