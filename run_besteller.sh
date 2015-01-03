ccdebfce5ce8c33f3bdf749a7bcdcb4b539aad2b
Markus Voge <sgrubsmyon@users.noreply.github.com>
2015-01-03 22:36:07 +0100

    Scripts added for fast development run (no jar file creation)

---
+++ run_besteller.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+build_dir=build/besteller
+lib_dir=../../lib
+classpath=$lib_dir/mysql-connector-java-5.1.25-bin.jar:$lib_dir/jOpenDocument-1.3.jar:$lib_dir/date4j.jar:.
+main_class=Weltladenbesteller.Besteller
+
+cp config.properties $build_dir
+cp -r vorlagen $build_dir
+cd $build_dir
+java -cp $classpath $main_class
+rm config.properties
+rm -r vorlagen
