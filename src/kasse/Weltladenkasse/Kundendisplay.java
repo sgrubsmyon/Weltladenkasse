c2f6ab0ad8c731ff8e9114d9d0447dab9932d836
Markus Voge <sgrubsmyon@users.noreply.github.com>
2015-05-15 11:42:37 +0200

    Tried to send ESC sequences to BA63 USB customer display

--- src/kasse/Weltladenkasse/Kundendisplay.java
+++ src/kasse/Weltladenkasse/Kundendisplay.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 // HID API to talk with Nixdorf BA63/USB display:
 import com.codeminders.hidapi.*;
 
-import WeltladenDB.JNIFromJar;
+//import WeltladenDB.JNIFromJar;
 
 /**
  * This class demonstrates enumeration, reading and getting
@@ -16,22 +16,27 @@ public class Kundendisplay {
     private static final long READ_UPDATE_DELAY_MS = 50L;
 
     static {
-        try {
-            System.loadLibrary("hidapi-jni"); // used for tests. This library in classpath only
-        } catch (UnsatisfiedLinkError e) {
-            try {
-                JNIFromJar.loadLibraryFromJar("/resources/natives/"+System.mapLibraryName("hidapi-jni-32"));
-                                                // during runtime. .DLL within .JAR
-            } catch (IOException e1) {
-                throw new RuntimeException(e1);
-            }
-        }
+        ClassPathLibraryLoader.loadNativeHIDLibrary();
+
+        // or:
+
+        //try {
+        //    System.loadLibrary("hidapi-jni"); // used for tests. This library in classpath only
+        //} catch (UnsatisfiedLinkError e) {
+        //    try {
+        //        JNIFromJar.loadLibraryFromJar("/resources/natives/"+System.mapLibraryName("hidapi-jni-32"));
+        //                                        // during runtime. .DLL within .JAR
+        //    } catch (IOException e1) {
+        //        throw new RuntimeException(e1);
+        //    }
+        //}
     }
 
     // Wincor Nixdorf International GmbH Operator Display, BA63-USB
     static final int VENDOR_ID = 2727;
     static final int PRODUCT_ID = 512;
-    private static final int BUFSIZE = 2048;
+    //private static final int BUFSIZE = 2048;
+    private static final int BUFSIZE = 32;
 
     /**
      *    The constructor.
@@ -47,41 +52,73 @@ public class Kundendisplay {
     private static void readDevice() {
         HIDDevice dev;
         try {
-            HIDManager hid_mgr = HIDManager.getInstance();
-            dev = hid_mgr.openById(VENDOR_ID, PRODUCT_ID, null);
-            System.err.print("Manufacturer: " + dev.getManufacturerString() + "\n");
-            System.err.print("Product: " + dev.getProductString() + "\n");
-            System.err.print("Serial Number: " + dev.getSerialNumberString() + "\n");
-            try {
-                byte[] buf = new byte[BUFSIZE];
-                dev.enableBlocking();
-                while(true)
-                {
-                    int n = dev.read(buf);
-                    for(int i=0; i<n; i++)
-                    {
-                        int v = buf[i];
-                        if (v<0) v = v+256;
-                        String hs = Integer.toHexString(v);
-                        if (v<16)
-                            System.err.print("0");
-                        System.err.print(hs + " ");
-                    }
-                    System.err.println("");
+            HIDManager manager = HIDManager.getInstance();
+            //dev = manager.openById(VENDOR_ID, PRODUCT_ID, null);
+            dev = manager.openByPath("0004:0003:00");
+            //dev = manager.openByPath("0004:0003:01");
+            System.out.print("Manufacturer: " + dev.getManufacturerString() + "\n");
+            System.out.print("Product: " + dev.getProductString() + "\n");
+            System.out.print("Serial Number: " + dev.getSerialNumberString() + "\n");
+            //try {
+            //    byte[] buf = new byte[BUFSIZE];
+            //    dev.enableBlocking();
+            //    while(true)
+            //    {
+            //        int n = dev.read(buf);
+            //        System.out.println(n);
+            //        for(int i=0; i<n; i++)
+            //        {
+            //            int v = buf[i];
+            //            if (v<0) v = v+256;
+            //            String hs = Integer.toHexString(v);
+            //            if (v<16)
+            //                System.out.print("0");
+            //            System.out.print(hs + " ");
+            //        }
+            //        System.out.println("");
 
-                    try
-                    {
-                        Thread.sleep(READ_UPDATE_DELAY_MS);
-                    } catch(InterruptedException e)
-                    {
-                        //Ignore
-                        e.printStackTrace();
-                    }
+            //        try
+            //        {
+            //            Thread.sleep(READ_UPDATE_DELAY_MS);
+            //        } catch(InterruptedException e)
+            //        {
+            //            //Ignore
+            //            e.printStackTrace();
+            //        }
+            //    }
+            try {
+                byte[] data = new byte[]{0x01, 0x1b, 0x5b, 0x30, 0x63};
+                //byte[] data = new byte[]{0x00, 0x00, 0x10, 0x00, 0x00,
+                //    0x00, 0x00, 0x00, 0x00,
+                //    0x00, 0x00, 0x00, 0x00,
+                //    0x00, 0x00, 0x00, 0x00,
+                //    0x00, 0x00, 0x00, 0x00,
+                //    0x00, 0x00, 0x00, 0x00,
+                //    0x00, 0x00, 0x00, 0x00,
+                //    0x00, 0x00, 0x00, 0x00};
+                int n = dev.write(data);
+                System.out.println("Number of bytes written to BA63 device: "+n);
+
+                byte[] buf = new byte[BUFSIZE];
+                //dev.enableBlocking();
+                n = dev.readTimeout(buf, 10000);
+                System.out.println("Number of bytes read: "+n);
+                //dev.disableBlocking();
+                for(int i=0; i<n; i++)
+                {
+                    int v = buf[i];
+                    if (v<0) v = v+256;
+                    String hs = Integer.toHexString(v);
+                    if (v<16)
+                        System.out.print("0");
+                    System.out.print(hs + " ");
                 }
-            } finally
-            {
+                System.out.println("");
+            } finally {
                 dev.close();
-                hid_mgr.release();
+                if (null != manager){
+                    manager.release();
+                }
                 System.gc();
             }
 
@@ -99,17 +136,17 @@ public class Kundendisplay {
     private static void listDevices()
     {
         String property = System.getProperty("java.library.path");
-        System.err.println(property);
+        System.out.println(property);
+        HIDManager manager = null;
         try
         {
-
-            HIDManager manager = HIDManager.getInstance();
+            manager = HIDManager.getInstance();
             HIDDeviceInfo[] devs = manager.listDevices();
-            System.err.println("Devices:\n\n");
+            System.out.println("Devices:");
             for(int i=0; i<devs.length; i++)
             {
-                System.err.println(""+i+".\t"+devs[i]);
-                System.err.println("---------------------------------------------\n");
+                System.out.println(""+i+":\t"+devs[i]);
+                System.out.println("---------------------------------------------\n");
             }
             System.gc();
         }
@@ -117,6 +154,17 @@ public class Kundendisplay {
         {
             System.err.println(e.getMessage());
             e.printStackTrace();
+        }
+        catch(NullPointerException e)
+        {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            System.err.println("It seems that the customer display is not connected. (No HID devices found.)");
+        } finally {
+            if (null != manager){
+                manager.release();
+            }
+            System.gc();
         }
     }
 
