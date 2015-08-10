9c2b85087f19a15a5d8ab9002fcf610d75deb1a6
Markus Voge <sgrubsmyon@users.noreply.github.com>
2015-08-10 02:15:30 +0200

    Created script to grant remote users access to MySQL DB

--- mysql/generateDB.sql
+++ mysql/generateDB.sql
@@ -179,20 +179,4 @@ CREATE TABLE bestellung_details (
     FOREIGN KEY (artikel_id) REFERENCES artikel(artikel_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-GRANT ALL PRIVILEGES ON kasse.* TO 'kassenadmin'@'localhost';
-GRANT SELECT ON kasse.* TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE ON kasse.artikel TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE ON kasse.mwst TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE ON kasse.lieferant TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE ON kasse.produktgruppe TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE ON kasse.verkauf TO 'mitarbeiter'@'localhost';
-GRANT INSERT ON kasse.verkauf_mwst TO 'mitarbeiter'@'localhost';
-GRANT INSERT ON kasse.verkauf_details TO 'mitarbeiter'@'localhost';
-GRANT INSERT ON kasse.kassenstand TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE ON kasse.rabattaktion TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE ON kasse.pfand TO 'mitarbeiter'@'localhost';
-GRANT INSERT ON kasse.abrechnung_tag TO 'mitarbeiter'@'localhost';
-GRANT INSERT, DELETE ON kasse.abrechnung_monat TO 'mitarbeiter'@'localhost';
-GRANT INSERT, DELETE ON kasse.abrechnung_jahr TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON kasse.bestellung TO 'mitarbeiter'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON kasse.bestellung_details TO 'mitarbeiter'@'localhost';
+SOURCE grants.sql;
