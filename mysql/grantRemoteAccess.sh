f8ae9d5a1585126646e70e8430b0f4937c42157b
Markus Voge <sgrubsmyon@users.noreply.github.com>
2015-08-10 02:15:30 +0200

    Created script to grant remote users access to MySQL DB

---
+++ mysql/grantRemoteAccess.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+if [ -n "$1" ]; then
+    root_pwd=$1
+fi
+
+# Check if MySQL root password is correct
+while : ; do
+    if [ -z "$root_pwd" ]; then
+        read -s -p "Enter MySQL root password: " root_pwd; echo
+    fi
+    mysql -h localhost -u root -p$root_pwd --execute="SELECT 1;" > /dev/null
+    if [[ $? -eq 0 ]]; then
+        break
+    else
+        echo "Wrong password. Try again."
+        root_pwd=""
+    fi
+done
+
+read -p "Enter remote hostname that shall be allowed remote access to this server: " host; echo
+
+# get new passwords from user
+while : ; do
+    read -s -p "Enter new admin password for MySQL user 'kassenadmin'@'$host': " admin_pwd_1; echo
+    read -s -p "Enter password again: " admin_pwd_2; echo
+    if [[ "$admin_pwd_1" == "$admin_pwd_2" ]]; then
+        break
+    else
+        echo "Passwords do not match. Try again."
+    fi
+done
+admin_pwd=$admin_pwd_1
+echo ""
+while : ; do
+    read -s -p "Enter new user password for MySQL user 'mitarbeiter'@'$host': " mitar_pwd_1; echo
+    read -s -p "Enter password again: " mitar_pwd_2; echo
+    if [[ "$mitar_pwd_1" == "$mitar_pwd_2" ]]; then
+        break
+    else
+        echo "Passwords do not match. Try again."
+    fi
+done
+mitar_pwd=$mitar_pwd_1
+
+sed "s/localhost/$host/" grants.sql > grants_temp.sql
+
+# create user admin and user mitarbeiter
+# create DB, grant access rights, and create tables
+echo ""
+echo "Will now create the remote MySQL users..."
+mysql --local-infile -h localhost -u root -p$root_pwd --execute="
+GRANT USAGE ON *.* TO 'kassenadmin'@'$host';
+GRANT USAGE ON *.* TO 'mitarbeiter'@'$host';
+DROP USER 'kassenadmin'@'$host';
+DROP USER 'mitarbeiter'@'$host';
+CREATE USER 'kassenadmin'@'$host' IDENTIFIED BY '$admin_pwd';
+CREATE USER 'mitarbeiter'@'$host' IDENTIFIED BY '$mitar_pwd';
+SOURCE grants_temp.sql;
+"
+stats=$?
+
+rm grants_temp.sql
+
+echo ""
+if [[ $stats -eq 0 ]]; then
+    echo "Looks like the users were created without any errors!"
+else
+    echo "Seems there was a problem. Please check any error messages and try to solve the problem or ask for help."
+fi
+
+exit 0
---
+++ mysql/grants.sql
@@ -0,0 +1,17 @@
+GRANT ALL PRIVILEGES ON kasse.* TO 'kassenadmin'@'localhost';
+GRANT SELECT ON kasse.* TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE ON kasse.artikel TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE ON kasse.mwst TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE ON kasse.lieferant TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE ON kasse.produktgruppe TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE ON kasse.verkauf TO 'mitarbeiter'@'localhost';
+GRANT INSERT ON kasse.verkauf_mwst TO 'mitarbeiter'@'localhost';
+GRANT INSERT ON kasse.verkauf_details TO 'mitarbeiter'@'localhost';
+GRANT INSERT ON kasse.kassenstand TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE ON kasse.rabattaktion TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE ON kasse.pfand TO 'mitarbeiter'@'localhost';
+GRANT INSERT ON kasse.abrechnung_tag TO 'mitarbeiter'@'localhost';
+GRANT INSERT, DELETE ON kasse.abrechnung_monat TO 'mitarbeiter'@'localhost';
+GRANT INSERT, DELETE ON kasse.abrechnung_jahr TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE, DELETE ON kasse.bestellung TO 'mitarbeiter'@'localhost';
+GRANT INSERT, UPDATE, DELETE ON kasse.bestellung_details TO 'mitarbeiter'@'localhost';
