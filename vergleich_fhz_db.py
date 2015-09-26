83faa18d90d0704d9ecfc4fd31d1d06cbd15a85f
Markus Voge <markus.voge@ymail.com>
2015-09-26 13:42:18 +0200

    More work on FHZ comparison

--- vergleich_fhz_db.py
+++ vergleich_fhz_db.py
@@ -5,8 +5,9 @@ import numpy as np
 import pandas as pd
 
 # load data
-fhz = pd.read_csv('Artikelliste_Bestellvorlage_Lebensmittelpreisliste_1.2-2015.csv', sep=';')
-wlb = pd.read_csv('Artikelliste_DB_Dump_2015_KW39.csv', sep=';')
+fhz = pd.read_csv('Artikelliste_Bestellvorlage_Lebensmittelpreisliste_1.2-2015.csv',
+        sep=';', dtype=str)
+wlb = pd.read_csv('Artikelliste_DB_Dump_2015_KW39.csv', sep=';', dtype=str)
 
 # homogenize data
 print(set(fhz['Lieferant']))
@@ -22,26 +23,26 @@ print(set(wlb['Lieferant']))
 #wlb_nummer_lower = np.array(list(map(lambda s: str(s).lower(), wlb.Artikelnummer.values)))
 count = 0
 for i in range(len(fhz)):
-    fhz_preis = fhz['Empf. VK-Preis'][i]
+    fhz_preis = float(fhz['Empf. VK-Preis'][i])
     #i_wlb = (wlb.Lieferant == fhz.Lieferant[i]) & (wlb_nummer_lower == str(fhz.Artikelnummer[i]).lower())
     i_wlb = (wlb.Lieferant == fhz.Lieferant[i]) & (wlb.Artikelnummer == fhz.Artikelnummer[i])
     wlb_preis = wlb['VK-Preis'][i_wlb].values
-    wlb_preis = wlb_preis[0] if len(wlb_preis) > 0 else np.nan
-    if ( np.isnan(fhz_preis) or np.isnan(wlb_preis) or abs(fhz_preis - wlb_preis) > 0.01 ):
+    wlb_preis = float(wlb_preis[0]) if len(wlb_preis) > 0 else np.nan
+    if ( np.isnan(fhz_preis) or np.isnan(wlb_preis) or abs(fhz_preis - wlb_preis) > 0.011 ):
         count += 1
         print('FHZ:', fhz_preis, 'WLB:', wlb_preis,
                 '('+str(fhz['Bezeichnung | Einheit'][i]).replace('\n',' ')+')',
                 '('+str(wlb['Bezeichnung | Einheit'][i_wlb]).replace('\n',' ')+')')
-print(count, "Differenzen gefunden.")
+print(count, "Differenzen gefunden.\n")
 
 # Adopt values from FHZ (for existing articles):
 wlb_neu = wlb.copy()
 for i in range(len(fhz)):
-    fhz_preis = fhz['Empf. VK-Preis'][i]
+    fhz_preis = float(fhz['Empf. VK-Preis'][i])
     #i_wlb = (wlb_neu.Lieferant == fhz.Lieferant[i]) & (wlb_nummer_lower == str(fhz.Artikelnummer[i]).lower())
     i_wlb = (wlb.Lieferant == fhz.Lieferant[i]) & (wlb.Artikelnummer == fhz.Artikelnummer[i])
     # adopt the rec. sale price and the "Lieferbarkeit" directly and completely
-    wlb_neu['Empf. VK-Preis'][i_wlb] = fhz_preis
+    wlb_neu['Empf. VK-Preis'][i_wlb] = str(fhz_preis)
     wlb_neu['Sofort lieferbar'][i_wlb] = fhz['Sofort lieferbar'][i]
     # adopt the sale price only if significantly changed:
     wlb_preis = wlb['VK-Preis'][i_wlb].values
@@ -49,11 +50,25 @@ for i in range(len(fhz)):
         print("Multiple articles with Lieferant %s and Nummer %s" %
                 (fhz.Lieferant[i], fhz.Artikelnummer[i]))
         sys.exit()
-    wlb_preis = wlb_preis[0] if len(wlb_preis) > 0 else np.nan
-    if ( abs(fhz_preis - wlb_preis) > 0.02 ):
+    wlb_preis = float(wlb_preis[0]) if len(wlb_preis) > 0 else np.nan
+    if ( fhz_preis >= 2.*wlb_preis ):
+        # price is at least twice, this indicates that this is a set:
+        print("WLB price:", wlb_preis, "FHZ price:", fhz_preis,
+                "(%s)" % fhz['Bezeichnung | Einheit'][i])
+        setgroesse = round(fhz_preis / wlb_preis)
+        fhz_preis = round(fhz_preis / setgroesse, 2)
+        old_setgroesse = wlb['Setgröße'][i_wlb].values
+        old_setgroesse = old_setgroesse[0] if len(old_setgroesse) > 0 else np.nan
+        if (setgroesse != old_setgroesse):
+            print("Old (WLB) setgroesse:", old_setgroesse,
+                    "   New guessed setgroesse:", setgroesse)
+            print("Changing setgroesse from:", old_setgroesse, " to:",
+                    setgroesse)
+            wlb_neu['Setgröße'][i_wlb] = str(setgroesse)
+    if ( abs(fhz_preis - wlb_preis) > 0.011 ):
         # price seems to deviate more than usual
         print("Old (WLB) price:", wlb_preis, "   New (FHZ) price:", fhz_preis,
-        "(%s)" % fhz['Bezeichnung | Einheit'][i])
+                "(%s)" % fhz['Bezeichnung | Einheit'][i])
         fhz_preis_str = str(fhz_preis)
         cent_index = fhz_preis_str.index('.') + 2
         if ( (len(fhz_preis_str) > cent_index) and
@@ -61,30 +76,44 @@ for i in range(len(fhz)):
             # round up, i.e. add one cent:
             fhz_preis += 0.01
         print("Changing price from:", wlb_preis, " to:", fhz_preis)
-        wlb_neu['VK-Preis'][i_wlb] = fhz_preis
+        wlb_neu['VK-Preis'][i_wlb] = str(fhz_preis)
+    print("\n")
 
-
-# Add new, so far unknown articles form FHZ:
-
-'''
 count = 0
 for i in range(len(fhz)):
-    fhz_preis = fhz['Empf. VK-Preis'][i]
+    fhz_preis = float(fhz['Empf. VK-Preis'][i])
     #i_wlb = (wlb_neu.Lieferant == fhz.Lieferant[i]) & (wlb_nummer_lower == str(fhz.Artikelnummer[i]).lower())
     i_wlb = (wlb.Lieferant == fhz.Lieferant[i]) & (wlb.Artikelnummer == fhz.Artikelnummer[i])
     wlb_preis = wlb_neu['Empf. VK-Preis'][i_wlb].values
-    wlb_preis = wlb_preis[0] if len(wlb_preis) > 0 else np.nan
-    if ( np.isnan(fhz_preis) or np.isnan(wlb_preis) or abs(fhz_preis - wlb_preis) > 0.01 ):
+    wlb_preis = float(wlb_preis[0]) if len(wlb_preis) > 0 else np.nan
+    if ( not np.isnan(fhz_preis) and not np.isnan(wlb_preis) and abs(fhz_preis -
+        wlb_preis) > 0.011 ):
         count += 1
         print('FHZ:', fhz_preis, 'WLB:', wlb_preis,
                 '('+str(fhz['Bezeichnung | Einheit'][i]).replace('\n',' ')+')',
                 '('+str(wlb_neu['Bezeichnung | Einheit'][i_wlb]).replace('\n',' ')+')')
 print(count, "Differenzen gefunden.")
-'''
 
 wlb_neu.to_csv('test.csv', sep=';', index=False)
-#    if ( np.isnan(fhz_preis) or np.isnan(wlb_preis) or abs(fhz_preis - wlb_preis) > 0.01):
+#    if ( np.isnan(fhz_preis) or np.isnan(wlb_preis) or abs(fhz_preis - wlb_preis) > 0.011):
 #        print('FHZ:', fhz.iloc[i])
 #fhz['Bezeichnung | Einheit'][5]
 #fhz.iloc[5]
+
+# Add new, so far unknown articles from FHZ:
+wlb_neue_artikel = pd.DataFrame()
+for i in range(len(fhz)):
+    fhz_preis = float(fhz['Empf. VK-Preis'][i])
+    #i_wlb = (wlb.Lieferant == fhz.Lieferant[i]) & (wlb_nummer_lower == str(fhz.Artikelnummer[i]).lower())
+    i_wlb = (wlb.Lieferant == fhz.Lieferant[i]) & (wlb.Artikelnummer == fhz.Artikelnummer[i])
+    wlb_preis = wlb['VK-Preis'][i_wlb].values
+    if (len(wlb_preis) == 0):
+        wlb_neue_artikel = wlb_neue_artikel.add(fhz.iloc[i])
+        fhz_preis_str = str(fhz_preis)
+        cent_index = fhz_preis_str.index('.') + 2
+        if ( (len(fhz_preis_str) > cent_index) and
+                (fhz_preis_str[cent_index] == '9') ):
+            # round up, i.e. add one cent:
+            fhz_preis += 0.01
+        wlb_neue_artikel['VK-Preis'][-1] = str(fhz_preis)
 
