c843b184b3bf715d2d04fb1317cc71f5ca1ede02
Markus Voge <markus.voge@ymail.com>
2015-10-24 18:38:07 +0200

    Found a solution for training mode

--- make_release.sh
+++ make_release.sh
@@ -2,9 +2,13 @@
 
 version=v0.9.9
 
-releasedir=../releases/Weltladenkasse_$version
+releasedir=../releases/Weltladenkasse_${version}
+trainingdir=../releases/Weltladenkasse_${version}_training
 if [ ! -e $releasedir ]; then
     mkdir -p $releasedir
+fi
+if [ ! -e $trainingdir ]; then
+    mkdir -p $trainingdir
 fi
 rsync -aPvci Weltladenkasse_$version.jar $releasedir
 rsync -aPvci Weltladenbesteller_$version.jar $releasedir
@@ -16,6 +20,16 @@ rsync -aPvci --delete --exclude=".*" vorlagen $releasedir
 rsync -aPvci README.md $releasedir
 rsync -aPvci install-ubuntu.sh $releasedir
 rsync -aPvci --delete --exclude=".*" mysql $releasedir
+rsync -aPvci Weltladenkasse_$version.jar $trainingdir
+rsync -aPvci Weltladenbesteller_$version.jar $trainingdir
+rsync -aPvci Weltladenkasse.bat $trainingdir
+rsync -aPvci Weltladenbesteller.bat $trainingdir
+rsync -aPvci config_local.properties $trainingdir
+rsync -aPvci config_Windows.properties $trainingdir
+rsync -aPvci --delete --exclude=".*" vorlagen $trainingdir
+rsync -aPvci README.md $trainingdir
+rsync -aPvci install-ubuntu.sh $trainingdir
+rsync -aPvci --delete --exclude=".*" mysql $trainingdir
 
 cd $releasedir
 if [ -e ../Weltladenkasse_$version.zip ]; then
