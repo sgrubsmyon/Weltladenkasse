7b08c17b82c62f8de7ef13386585425f0dbaa3ef
Markus Voge <markus.voge@ymail.com>
2015-11-02 02:38:38 +0100

    Canged version to v1.0.0 for first regular usage of Kassieren

--- Weltladenbesteller.bat
+++ Weltladenbesteller.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenbesteller_v0.9.9.jar
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenbesteller_v1.0.0.jar
--- Weltladenkasse.bat
+++ Weltladenkasse.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenkasse_v0.9.9.jar
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenkasse_v1.0.0.jar
