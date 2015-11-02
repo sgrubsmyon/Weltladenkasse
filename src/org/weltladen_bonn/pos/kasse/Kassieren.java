bd351efafb989d7b2bfed2c826c781b4f82097b3
Markus Voge <markus.voge@ymail.com>
2015-11-02 02:06:42 +0100

    Probably solved the Pfandkiste problem

--- src/org/weltladen_bonn/pos/kasse/Kassieren.java
+++ src/org/weltladen_bonn/pos/kasse/Kassieren.java
@@ -1882,11 +1882,11 @@ public class Kassieren extends RechnungsGrundlage implements ArticleSelectUser, 
             return;
         }
         if (e.getSource() == sevenPercentButton) {
-            setSelectedArticle(10); // Variabler Preis 7%
+            setSelectedArticle(11); // Variabler Preis 7%
             return;
         }
         if (e.getSource() == nineteenPercentButton) {
-            setSelectedArticle(11); // Variabler Preis 19%
+            setSelectedArticle(12); // Variabler Preis 19%
             return;
         }
         if (e.getSource() == hinzufuegenButton) {
