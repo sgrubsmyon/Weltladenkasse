ae4391994b5168a9949958e5fd1c4313e8a869c9
Markus Voge <mvoge@posteo.de>
2017-03-08 00:45:32 +0100

    Removed dependency on aptitude in install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Check if mysql is already installed via aptitude:
-pm_check() {
-    search_result=$(aptitude search \^mysql-server\$)
-    search_result=${search_result:0:1}
-    echo $search_result
-    #return $search_result
+# Check if mysql is already installed via package manager:
+installed_via_pm() {
+  dpkg --get-selections | grep mysql-server > /dev/null
+  if [ $? == 0 ]; then
+    echo "yes"
+  else
+    echo "no"
+  fi
 }
 
 # Check if mysql is in the $PATH, argument can be mysql root password:
@@ -38,8 +40,7 @@ print_problem_help() {
 }
 
 echo -n "Checking if MySQL is installed via package manager... "
-search_result=$(pm_check)
-if [[ $search_result == "i" ]]; then echo "yes"; else echo "no"; fi
+echo $(installed_via_pm)
 read -s -p "Enter MySQL root password: " root_pwd; echo
 echo -n "Checking if MySQL admin tool is executable... "
 mysqladmin_executable=$(path_check $root_pwd)
@@ -48,25 +49,24 @@ echo -n "Checking if MySQL works... "
 mysql_works=$(run_check $root_pwd)
 if [[ $mysql_works -eq 0 ]]; then echo "yes"; else echo "no"; fi
 
-if [[ $search_result != "i" && $mysql_works -ne 0 ]]; then
+if [[ $(installed_via_pm) != "yes" && $mysql_works -ne 0 ]]; then
     echo -e "MySQL seems to be not installed. Trying to install it now (need root privilieges)."
     sudo apt-get install mysql-server
-    search_result=$(pm_check)
     read -s -p "Enter MySQL root password: " root_pwd; echo
     echo -n "Checking if MySQL works... "
     mysql_works=$(run_check $root_pwd)
-    if [[ $search_result == "i" && $mysql_works -eq 0 ]]; then
+    if [[ $(installed_via_pm) == "yes" && $mysql_works -eq 0 ]]; then
         echo -e "MySQL is now successfully installed."
     else
         echo -e "There was an error during the MySQL installation \n(or you entered wrong root password)."
         print_problem_help
         exit 1
     fi
-elif [[ $search_result != "i" && $mysql_works -eq 0 ]]; then
+  elif [[ $(installed_via_pm) != "yes" && $mysql_works -eq 0 ]]; then
     echo -e "MySQL seems to have been installed manually, not via package manager.\nTrying to use this mysql install."
-elif [[ $search_result == "i" && $mysql_works -eq 0 ]]; then
+  elif [[ $(installed_via_pm) == "yes" && $mysql_works -eq 0 ]]; then
     echo -e "MySQL seems to be installed via package manager and working."
-elif [[ $search_result == "i" && $mysql_works -ne 0 ]]; then
+  elif [[ $(installed_via_pm) == "yes" && $mysql_works -ne 0 ]]; then
     echo -e "MySQL seems to be installed via package manager, but it is not working \n(or you entered wrong root password)."
     print_problem_help
     exit 1
