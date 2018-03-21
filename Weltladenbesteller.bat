2fe512e8cd0593f456e03f9e05b7827de75f7e74
Markus Voge <mvoge@posteo.de>
2018-03-22 00:19:20 +0100

    Make new release to be sure not to break anything

--- Weltladenbesteller.bat
+++ Weltladenbesteller.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenbesteller_v1.2.1.jar
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenbesteller_v1.2.2.jar
--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=v1.2.1
+version=v1.2.2
 
 releasedir=../releases/Weltladenkasse_${version}
 trainingdir=../releases/Weltladenkasse_${version}_training
