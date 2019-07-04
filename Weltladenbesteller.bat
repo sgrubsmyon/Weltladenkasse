17c3f38f8412884edcc4d863222e392dba7bf16e
Markus Voge <mvoge@posteo.de>
2019-07-04 14:10:12 +0200

    Version bump to 1.2.3

--- Weltladenbesteller.bat
+++ Weltladenbesteller.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenbesteller_v1.2.2.jar
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar Weltladenbesteller_v1.2.3.jar
--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=v1.2.2
+version=v1.2.3
 
 releasedir=../releases/Weltladenkasse_${version}
 trainingdir=../releases/Weltladenkasse_${version}_training
