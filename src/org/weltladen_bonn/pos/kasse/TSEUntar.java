dec4f5744fdc2630b5ef6572102415ed33961d24
Markus Voge <sgrubsmyon@users.noreply.github.com>
2020-12-29 17:52:11 +0100

    Add cryptovision helper code to extract TSE certificate

--- src/org/weltladen_bonn/pos/kasse/TSEUntar.java
+++ src/org/weltladen_bonn/pos/kasse/TSEUntar.java
@@ -3,6 +3,12 @@ package org.weltladen_bonn.pos.kasse;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+import com.cryptovision.SEAPI.TSE;
+import com.cryptovision.SEAPI.exceptions.SEException;
+
+import org.bouncycastle.util.encoders.Hex;
+
+/** https://tse-support.cryptovision.com/confluence/display/TDI/Die+Felder+TSE_ZERTIFIKAT_I+und+II+der+DSFinV-K */
 public class TSEUntar {
  
     public static ArrayList<TSETarFile> untar(byte[] tarArchive) {
@@ -32,4 +38,34 @@ public class TSEUntar {
         return tarList;
     }
 
+    public static ArrayList<TSETarFile> exportCertificatesAsList(TSE tse) throws SEException {
+        return untar(tse.exportCertificates());
+    }
+
+    public static byte[] hexToBytes(String hex)
+    {
+        hex = hex.replaceAll("[^A-Fa-f0-9]", "");
+        if (1 == hex.length() % 2)
+            hex = "0" + hex;
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < bytes.length; i++)
+            bytes[i] = (byte)((Character.digit(hex.charAt(i*2), 16) << 4) + Character.digit(hex.charAt(i*2+1), 16));
+        return bytes;
+    }
+ 
+    public static byte[] exportCertificate(TSE tse, byte[] serialNumberKey) throws SEException {
+        int len = 2 * serialNumberKey.length;
+        ArrayList<TSETarFile> tarList = exportCertificatesAsList(tse);
+        TSETarFile file;
+        for (int i = 0; i < tarList.size(); i++) {
+            file = tarList.get(i);
+            System.out.println("hexToBytes:" + hexToBytes(file.name.substring(0, len)));
+            System.out.println("hexStringToByteArray:" + WeltladenTSE.hexStringToByteArray(file.name.substring(0, len)));
+            if (
+                len < file.name.length() &&
+                Arrays.equals(serialNumberKey, hexToBytes(file.name.substring(0, len)))
+            ) return file.value;
+        }
+        throw new com.cryptovision.SEAPI.exceptions.ErrorTSECommandDataInvalid();
+    }
 }
