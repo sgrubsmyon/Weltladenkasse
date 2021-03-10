8f3d56d04714dd8b70ce4c45f4fd05abd766e9dd
Markus Voge <sgrubsmyon@users.noreply.github.com>
2021-03-10 23:52:18 +0100

    Insert new articles Anpassung, Anzahlung, Anzahlungsauflösung

--- mysql/fillWithInternalValues.sql
+++ mysql/fillWithInternalValues.sql
@@ -23,11 +23,11 @@ SOURCE interne_artikel.sql;
 
 LOCK TABLES `pfand` WRITE;
 /*!40000 ALTER TABLE `pfand` DISABLE KEYS */;
-INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (1,4);
-INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (2,5);
-INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (3,6);
-INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (4,7);
-INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (5,8);
-INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (6,9);
+INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (1,7);
+INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (2,8);
+INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (3,9);
+INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (4,10);
+INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (5,11);
+INSERT INTO `pfand` (`pfand_id`, `artikel_id`) VALUES (6,12);
 /*!40000 ALTER TABLE `pfand` ENABLE KEYS */;
 UNLOCK TABLES;
