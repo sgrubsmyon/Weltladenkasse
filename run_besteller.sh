214760dfe5b3d08ea51a7a7a0748c13aa2cd0ab2
Markus Voge <sgrubsmyon@users.noreply.github.com>
2021-11-13 23:59:15 +0100

    Need to use add_opens also in Besteller or...
    ...it won't run anymore with recent Java

--- run_besteller.sh
+++ run_besteller.sh
@@ -8,6 +8,8 @@ fi
 build_dir=build
 lib_dir=../lib
 main_class=org.weltladen_bonn.pos.besteller.Besteller
+# https://stackoverflow.com/questions/41265266/how-to-solve-inaccessibleobjectexception-unable-to-make-member-accessible-m
+add_opens="java.desktop/javax.swing.plaf.basic=ALL-UNNAMED"
 
 cp config_local.properties $build_dir/config.properties
 cp config_log4j2.xml $build_dir
@@ -18,9 +20,9 @@ if [ "$JAVA_HOME" != "" ]; then
     java="$JAVA_HOME/bin/java"
 fi
 if [ $runprofiler == true ]; then
-    $java -javaagent:$HOME/bin/profiler4j-1.0-beta2/agent.jar -cp "$lib_dir/*":. $main_class
+    $java -javaagent:$HOME/bin/profiler4j-1.0-beta2/agent.jar --add-opens "$add_opens" -cp "$lib_dir/*":. $main_class
 else
-    $java -cp "$lib_dir/*":. $main_class
+    $java --add-opens "$add_opens" -cp "$lib_dir/*":. $main_class
 fi
 rm config.properties
 rm config_log4j2.xml
