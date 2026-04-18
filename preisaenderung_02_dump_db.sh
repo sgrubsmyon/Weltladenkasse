e3f3c0655444849a20e962be6f612523bbc24f97
Markus Voge <sgrubsmyon@users.noreply.github.com>
2026-04-19 00:37:12 +0200

    New VPN setup means new SSH config server name

--- preisaenderung_02_dump_db.sh
+++ preisaenderung_02_dump_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -hlocalhost -ukassenadmin -p kasse -r DB_Dump_kasse_nach_preisaenderung.sql && rsync -aPvi DB_Dump_kasse_nach_preisaenderung.sql futon_vpn:.
+mysqldump -hlocalhost -ukassenadmin -p kasse -r DB_Dump_kasse_nach_preisaenderung.sql && rsync -aPvi DB_Dump_kasse_nach_preisaenderung.sql futon:.
